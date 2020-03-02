71579eb3141a645b460b5498f2aa30f0a41992c1
Chu Van Tinh <chuvantinh1991@gmail.com>
2020-03-02 16:24:44 +0100

    change config of database to server webgo

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,27 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'tinhcv' );
+//define( 'DB_NAME', 'tinhcv' );
+//
+///** MySQL database username */
+//define( 'DB_USER', 'root' );
+//
+///** MySQL database password */
+//define( 'DB_PASSWORD', 'root' );
+//
+///** MySQL hostname */
+//define( 'DB_HOST', 'localhost' );
+
+define( 'DB_NAME', 'web74_db7' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'web74_7' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'CfzhrOInweKbVTQy' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 's159.goserver.host' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
