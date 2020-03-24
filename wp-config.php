8bc72ebbe0ed5205b4dd2df550756e63171bb967
Chu Van Tinh <chuvantinh1991@gmail.com>
2020-03-24 11:49:02 +0100

    worked on the first time

--- wp-config.php
+++ wp-config.php
@@ -20,33 +20,39 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-//define( 'DB_NAME', 'tinhcv' );
+define( 'DB_NAME', 'tinhcv' );
 //
 ///** MySQL database username */
-//define( 'DB_USER', 'root' );
-//
-///** MySQL database password */
-//define( 'DB_PASSWORD', 'root' );
-//
-///** MySQL hostname */
-//define( 'DB_HOST', 'localhost' );
-
-define( 'DB_NAME', 'web74_db7' );
-
-/** MySQL database username */
-define( 'DB_USER', 'web74_7' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'CfzhrOInweKbVTQy' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 's159.goserver.host' );
+define( 'DB_HOST', 'localhost' );
 
-/** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+//// for server
+//define( 'DB_NAME', 'web74_db7' );
+//
+///** MySQL database username */
+//define( 'DB_USER', 'web74_7' );
+//
+///** MySQL database password */
+//define( 'DB_PASSWORD', 'CfzhrOInweKbVTQy' );
+//
+///** MySQL hostname */
+//define( 'DB_HOST', 's159.goserver.host' );
+
+///** Database Charset to use in creating database tables. */
+//define( 'DB_CHARSET', 'utf8mb4' );
+//
+///** The Database Collate type. Don't change this if in doubt. */
+//define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
