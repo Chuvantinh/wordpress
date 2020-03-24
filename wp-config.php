5bc787efaa5e7969de82de9e1ae3f3e65a7a5dc1
Chu Van Tinh <chuvantinh1991@gmail.com>
2020-03-24 12:23:35 +0100

    changed config into database

--- wp-config.php
+++ wp-config.php
@@ -48,11 +48,11 @@ define( 'DB_PASSWORD', 'web74_db7' );
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
 
-///** Database Charset to use in creating database tables. */
-//define( 'DB_CHARSET', 'utf8mb4' );
-//
-///** The Database Collate type. Don't change this if in doubt. */
-//define( 'DB_COLLATE', '' );
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
